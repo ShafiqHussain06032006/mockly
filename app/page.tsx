4aa8c3edd13e2a7301d06f8075c8543e4cd57102
Shafiq Hussain <shafiqhussain06032006@gmail.com>
2025-08-26 11:21:52 +0500

    Buttons and fonts are created

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import {Button} from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       hello my name is shafeeq hussain
+      <Button>click me</Button>
     </div>
   );
 }
