0cb3a2f7970efd3d13f05b06585235fa9c129a96
Shafiq Hussain <shafiqhussain06032006@gmail.com>
2025-08-26 14:20:45 +0500

    Landing Screen created successfully

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import { Button } from "@/components/ui/button"
+
 
 const Header = () => {
   return (
@@ -8,9 +10,7 @@ const Header = () => {
         <Image src={'/logo.svg'} alt='logo' width={40} height={40}  />
         <h1 className="text-base font-bold md:text-2xl">Mockly</h1>
       </div>
-      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-        Login
-      </button>
+      <Button >Get Started</Button>
     </nav>
   )
 }
