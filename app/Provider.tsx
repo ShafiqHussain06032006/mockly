2ef79759d181a789a873563e61c6f35232ccbd3c
Shafiq Hussain <shafiqhussain06032006@gmail.com>
2025-08-27 12:29:27 +0500

    Convex database is integrated

---
+++ app/Provider.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react'
+
+const Provider = ({children} : any) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Provider
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit} from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexClientProvider } from "./ConvexClientProvider";
 
 
 
@@ -26,7 +27,10 @@ export default function RootLayout({
       <body
         className={outfit.className}
       >
-        {children}
+        <ConvexClientProvider>
+               {children}
+        </ConvexClientProvider>
+     
       </body>
     </html>
     </ClerkProvider>
