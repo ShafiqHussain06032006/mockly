8340217a5cce1ad8bc20ba13f69c9abff94b4e7b
Shafiq Hussain <shafiqhussain06032006@gmail.com>
2025-08-31 14:10:46 +0500

    Interview creation page built successfully

--- app/(routes)/dashboard/EmptyState.tsx
+++ app/(routes)/dashboard/EmptyState.tsx
@@ -1,9 +1,17 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
 import React from 'react'
 
 const EmptyState = () => {
   return (
-    <div>
-      <div>Emptystate</div>
+    <div className='mt-14 flex flex-col  items-center gap-5 border border-dashed p-10 border-2'>
+      <Image src={'/interview.png'}  alt='emptyState' 
+      width={150}
+      height={150}
+      />
+
+      <h2 className='mt-2 text-lg text-gray-500' >You don't have any Interview created </h2>
+      <Button>Create Interview </Button>
     </div>
   )
 }
